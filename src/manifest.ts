719877a297810e32f26645757dbfe05777bd361c
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2025-02-05 10:42:11 +0100

    chore: update manifest version

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin';
 export default defineManifest({
   name: 'Notion Syntax Highlighter',
   description: "Improves Notion's code blocks syntax highlighting + adds tons of theme options",
-  version: '0.0.5',
+  version: '0.0.6',
   manifest_version: 3,
   permissions: ['storage'],
   icons: {
